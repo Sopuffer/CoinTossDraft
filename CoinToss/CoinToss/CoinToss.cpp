d435b0de85d552ee4288f1500f1c93e89f69793e
Breck1 <rundstrom.tony@gmail.com>
2021-06-23 18:37:07 +0200

    tested inputmanager

--- CoinToss/CoinToss/CoinToss.cpp
+++ CoinToss/CoinToss/CoinToss.cpp
@@ -2,13 +2,18 @@
 #include <iostream>
 #include <SDL.h>
 #include <SDL_image.h>
+#include "InputManager.h"
 using std::cout;
 using std::endl;
 
-
+int foo;
 
 int main(int argc, char** argv) {
 	cout << "Hello World" << endl;
+	InputManager* test = new InputManager();
+	cout << test->foo() << endl;
+	delete test;
+	test = nullptr;
 	if (SDL_IMAGE_COMPILEDVERSION)
 	{
 		cout << "hej2" << endl;
--- CoinToss/CoinToss/InputManager.h
+++ CoinToss/CoinToss/InputManager.h
@@ -2,7 +2,8 @@
 class InputManager
 {
 	public:
-
+		InputManager() {};
+		const int foo() { return 10; };
 	private:
 };
 
